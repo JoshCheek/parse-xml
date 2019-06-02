ae97fa71fb2ca81d0e91570fc486856326adad2a
Josh Cheek <josh.cheek@gmail.com>
2019-06-01 21:43:29 -0500

    parses a nested tag

--- lib/parse_xml.rb
+++ lib/parse_xml.rb
@@ -1,6 +1,6 @@
 require 'strscan'
 
-module ParseXml
+class ParseXml
   class Tag
     def initialize(name:, children:)
       @name = name
@@ -17,15 +17,97 @@ module ParseXml
   end
 
   def self.call(to_parse)
-    scanner = StringScanner.new to_parse
-    # scanner.scan /\s*/
-    scanner.scan /</
-    name = scanner.scan /\w+/
-    scanner.scan />/
-    scanner.scan /<\//
-    scanner.scan Regexp.new Regexp.escape name
-    scanner.scan />/
-    tag = Tag.new name: name, children: []
-    tag
+    new(tokenize to_parse).call
+  end
+
+  def self.tokenize(to_parse)
+    scanner = StringScanner.new(to_parse)
+    tokens  = []
+    chars   = "<>\"'=\\/"
+    regex   = /[#{chars}]|\s+|\w+|[^#{chars}\s\w]+/m
+    tokens << scanner.scan(regex) until scanner.eos?
+    tokens
+  end
+
+  def initialize(tokens)
+    @tokens = tokens
+    @index = 0
+  end
+
+  def call
+    @parsed ||= parse
+  end
+
+  private
+
+  def parse
+    children = parse_tags
+    if children.one?
+      children.first
+    else
+      raise 'root has multiple things'
+    end
+  end
+
+  def parse_tags
+    tags = []
+    while (tag = parse_tag)
+      tags << tag
+    end
+    tags
+  end
+
+  private def parse_tag
+    open do
+      opt_whitespace
+      expect '<'
+      opt_whitespace
+      name = expect /\A\w+\z/
+      opt_whitespace
+      expect '>'
+
+      children = parse_tags
+
+      opt_whitespace
+      expect '<'
+      opt_whitespace
+      expect '/'
+      expect name
+      expect '>'
+
+      Tag.new name: name, children: children
+    end
+  end
+
+  def open
+    index = @index
+    catch :fail do
+      result = yield
+      return result
+    end
+    @index = index
+    nil
+  end
+
+  def opt_whitespace
+    @index += 1 if token &.match?(/\A\s+\z/)
+  end
+
+  def token
+    @tokens[@index]
+  end
+
+  def expect(expected)
+    actual = token()
+    case expected
+    when String
+      throw :fail if actual != expected
+    when Regexp
+      throw :fail if actual !~ expected
+    else
+      raise "fixme: #{token.inspect}"
+    end
+    @index += 1
+    actual
   end
 end
